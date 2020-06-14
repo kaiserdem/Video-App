9bfaaa0b9188e6812f886b04592c7ded64d1ae5c
Yaroslav Golinskiy <kaiserdem@gmail.com>
2020-06-14 15:11:14 +0300

    initial xcode project

---
+++ Video-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video-App
+//
+//  Created by Kaiserdem on 14.06.2020.
+//  Copyright © 2020 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
